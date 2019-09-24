7f6bc69e340209e223a551408400850319fad322
taislopes <talopescontat@gmail.com>
2019-09-24 20:41:05 -0300

    exercicios de logica

--- 1_tipos_de_dados/exercicio.js
+++ 1_tipos_de_dados/exercicio.js
@@ -1,1 +1,14 @@
-//resposta aqui
+var cartao = "Tais"
+var nomeDoCliente = 'Tais';
+var idadeDoCliente = 25;
+var jaFrequentaALoja = true;
+var favoritos = ["Celular", "pizza", "sorvete"];
+
+var cartao = {
+    nomeDoCliente:nomeDoCliente,
+    idadeDoCliente: 25,
+    jaFrequentaALoja:jaFrequentaALoja,
+    favoritos:favoritos,
+}
+console.log (cartao);
+
--- 2_op_ar/exerciciocorrigido.js
+++ 2_op_ar/exerciciocorrigido.js
@@ -1,12 +1,11 @@
 var r = require ("readline-sync");
 
 var produtos = [
-    { nome: 'alface', preco:1.99 },
-    { nome:'cenoura', pereco:2.99 },
+    {nome: 'alface', preco:1.99},
+    {nome:'cenoura', pereco:2.99},
 ];
 
 var posicao = parseInt(r.question('Escolha um produto entre o e 2:'));
-
 var porcetagem = parseInt(r.question('digite uma porcetagem'));
 
 var produtoSelecionado = produtos [posicao];
@@ -14,6 +13,3 @@ var produtoSelecionado = produtos [posicao];
 var desconto = produtoSelecionado.preco * (porcetagem /100);
 
 console.log('Produto' + produtoSelecionado.nome + 'com desconto é de R$' + desconto.toFixed(2));
-
-
-
